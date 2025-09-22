65386ace100b9f13bf63be8c5077a9b1aad63326
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-09-23 01:31:21 +0530

    dsa

--- JAVA/LinkedList/Lecture1.java
+++ JAVA/LinkedList/Lecture1.java
@@ -1,7 +1,10 @@
 public class Lecture1{
     Node<Integer> head;
+    int size;
+
     Lecture1(){
         this.head = null;
+        this.size =0;
     }
     public static void main(String[] args) {
         
@@ -17,5 +20,74 @@ public class Lecture1{
             this.next =null;
             this.data = data;
         }
+    }
+
+
+
+
+
+    public void insertNode(int data , int position){
+
+        if(position>size+1 || position<=0){
+           System.out.println("wrong input");
+           return;
+        }
+        // head
+        if(position == 1){
+            insertHead(data);
+        
+        } else if(position ==size +1){
+            insertTail(data);
+            
+        }
+        else {
+            // at specific positions
+            int counter =1;
+            Node<Integer> newNode = new Node<>(data);
+            Node<Integer> temp = head;
+            while(counter<position-1 && temp != null){
+                counter++;
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+        System.out.println("Insertion successful at position"+position);
+    }
+
+    public void insertHead(int data ){
+        Node<Integer> newNode = new Node<>(data);
+        if(head == null){
+            head = newNode;
+           System.out.println("insetion at head is successful");
+           size++;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+        System.out.println("insetion at head is successful");
+        size++;
+    }
+
+
+
+    
+    public void insertTail(int data ){
+        Node<Integer> newNode = new Node<>(data);
+        if(head == null){
+            head = newNode;
+            System.out.println("insetion at tail is successful");
+            size++;            
+            return;
+        }
+
+        Node<Integer> temp = head;
+        while(temp.next!=null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        System.out.println("insetion at tail is successful");
+        size++;
     }
 }
