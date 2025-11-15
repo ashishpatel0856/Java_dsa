4e5ac54d10d80047b8868c48a076d9397c3c39b8
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-15 18:21:04 +0530

    linkedlist

---
+++ JAVA/LinkedList/basics1.java
@@ -0,0 +1,53 @@
+public class basics1{
+
+    public static class Node{
+        int data; //value
+        Node next; // address of next node
+        Node(int data){
+            this.data = data;
+        }
+    
+    }
+ 
+    public static void display(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node a = new Node(4);
+        Node b = new Node(41);
+        Node c = new Node(9);
+        Node d = new Node(3);
+        Node e = new Node(6);
+
+        a.next =b;
+        b.next =c;
+        c.next =d;
+        d.next=e;
+        // System.out.println(a.data);
+        // System.out.println(a.next.data);
+        // System.out.println(a.next.next.data);
+        // System.out.println(a.next.next.data);
+        // System.out.println(a.next.next.data);
+
+        Node temp = a;
+        // for(int i=1;i<=5;i++){
+        //     System.out.print(temp.data+"->");
+        //      // 4->41->9->6->3
+
+        //     temp = temp.next;
+           
+        //     }
+
+       
+        //  while(temp!=null){
+        //     System.out.print(temp.data+" ");
+        //     temp=temp.next;
+        // }
+        display(a);
+        
+    }
+}
