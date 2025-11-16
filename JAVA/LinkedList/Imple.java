f30edc268332ff7804d68bd83120465b52f0d80b
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-16 13:39:52 +0530

    add

--- JAVA/LinkedList/Imple.java
+++ JAVA/LinkedList/Imple.java
@@ -8,7 +8,15 @@ public class Imple{
         }
 
     }
-
+  
+    // public static void insertAtEndval(Node head,int val){
+    //   Node temp = new Node(val);
+    //   Node t = head;
+    //   while(t.next!=null){
+    //     t=t.next;
+    //   }
+    //   t.next=null;
+    // }
      
     public static class linkedlist{
        Node head = null;
@@ -27,14 +35,95 @@ public class Imple{
         tail = temp;
        }
 
+       void insertAtHead(int val){
+        Node temp = new Node(val);
+        if(head==null){//empty list
+        head =temp;
+        tail = temp;
+        } else{ // non empty
+           temp.next = head;
+           head =temp;
+        }
+       }
+
+
+       void insertAtPosition(int idx,int val){
+          Node t = new Node(val);
+          Node temp = head;
+          if(idx==size()){
+            insertAtEnd(val);
+            return;
+          } else if(idx ==0){
+            insertAtHead(val);
+            return ;
+          } else if(idx<0){
+            System.out.println("wrong idx");
+            return;
+          } else if(idx>size()){
+            System.out.println("wrong idx");
+            return;
+          }
+
+
+          for(int i=1;i<idx-1;i++){
+            temp = temp.next;
+          }
+          t.next = temp.next;
+          temp.next=t;
+
+       }
+
+
+    int size(){
+    int count = 0;
+    Node temp = head;
+    while(temp != null){
+        count++;
+        temp = temp.next;
+    }
+    return count;
+   }
+
+
+  int getAtval(int idx){
+    Node temp = head;
+    for(int i=1;i<=idx;i++){
+      temp = temp.next;
+    }
+    return temp.data;
+  }
+
+
+
+
+  //  deleting the value in linkedlist
+
+  void deleteAt(int idx){
+   
+    Node temp = head;
+    int size;
+     if(idx==0){
+      head = head.next;
+      size=size()-1;
+    }
+    for(int i =1;i<=idx-1;i++){
+      temp = temp.next;
+    }
+    temp.next = temp.next.next;
+    tail=temp;
+    size=size()-1;
+  }
+
        void display(){
         Node temp = head;
         while(temp!=null){
-            System.out.println(temp.data+" ");
+            System.out.print(temp.data+" ");
             temp = temp.next;
         }
        }
+       
     }
+     
     public static void main(String[] args) {
       linkedlist ll = new linkedlist();
       ll.insertAtEnd(7);
@@ -44,5 +133,19 @@ public class Imple{
        ll.insertAtEnd(20);
        System.out.println("");
        ll.display();
+       System.out.println("");
+       ll.insertAtHead(91);
+       ll.display();
+     System.out.println("");
+     ll.insertAtPosition(5, 50);
+     ll.display();
+     System.err.println("");
+     System.out.println(ll.getAtval(3));//10
+     System.err.println("");
+     ll.deleteAt(0);
+     ll.display();
+    
+
+
     }
 }
