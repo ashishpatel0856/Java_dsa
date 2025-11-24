dde65688a50b8e695733a7eb362d7f2b04a284f0
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-11-24 17:52:12 +0530

    add

---
+++ JAVA/Strings/Basics.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str ="ashish and karina";
+        // System.out.println(str);
+        // String s = sc.next();
+        // String s = sc.nextLine(); // with spaces 
+        // System.out.println(s);
+
+        // String str = "Karina";
+        // System.err.println(str.length());
+
+    //    char ch = str.charAt(1);
+    //    System.out.println(ch);
+
+    // int idx = str.indexOf('a');// retrun always first accurance
+    // System.out.println(idx);
+
+
+    // compareTo()
+
+    // String gtr = "Zshish";
+    // System.out.print(str.compareTo(gtr));// K-A=10 , K-Z=-15
+
+
+
+    // String str = "ashish patel";
+    // System.out.println(str.contains("s")); // true
+    // System.out.print(str.endsWith("tel"));
+
+
+    }
+}
