eb85be6e15081b6f065e0f9e29f6b88d755e7a08
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-12-07 15:39:10 +0530

    dsa

--- JAVA/Stacks/ArrayImplementations.java
+++ JAVA/Stacks/ArrayImplementations.java
@@ -1,66 +1,80 @@
 public class ArrayImplementations {
-    int stack[];
-    int top;
-    int size;
-    public ArrayImplementations(int stackSize){
-        stack = new int[stackSize];
-        size = stackSize;
-        top =-1;
-    }
-    // inserting elements in stacks
+ 
+    int[] arr = new int[4];
+    int idx =0;
 
-    public void push(int elements){
-        if(top>=size-1){
-            System.out.println("stack is full - overflow");
-            return;
+    void push(int x){
+        if(idx == arr.length){
+            System.out.print("stack is overflow");
         }
-        top++;
-        stack[top]=elements;
-    }
-    
-
-    public boolean isEmpty(){
-        return top ==-1;
+        arr[idx] =x;
+        idx++;
     }
 
-    public int size() {
-        return top+1;
-    }
-
-    public int peek() {
-        if(isEmpty()){
-            return Integer.MIN_VALUE;
+    int peek(){
+        if(idx==0){
+            System.out.println("stack is empty");
+            return -1;
+        } else {
+            return arr[idx-1];
         }
-        return stack[top];
     }
 
-    public int pop() {
-        int val = peek();
-        if(val != Integer.MIN_VALUE){
-            top--;
-        }
-        return val;
+    int pop(){
+        if(idx ==0) {
+            System.out.print("stack is underflow");
+            return -1;
+        } 
+        int top = arr[idx-1];
+        idx--;
+        return top; 
+        
     }
 
-    public void printStack(){
-        System.out.println("printing stack");
-        for(int i=0;i<=top;i++){
-           System.out.print(stack[i]);
-           if(i!=top){
-            System.out.print(",");
-           }
+
+    int isCapacity(){
+        return arr.length;
+    }
+
+    boolean isEmpty(){
+        if(idx==0) return true;
+        else return false;
+    }
+
+    boolean isFull(){
+        if(idx == arr.length){
+            return true;
+        } 
+        return  false;
+    }
+
+    int size(){
+        return idx;
+    }
+    void display(){
+        for(int i =0;i<idx;i++){
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
+    
+   
+    
     public static void main(String[] args){
-      int n =5;
-      ArrayImplementations stack = new ArrayImplementations(n);
+    
+      ArrayImplementations stack = new ArrayImplementations();
+      System.err.println(stack.isEmpty());
       stack.push(90);
       stack.push(10);
       stack.push(23);
-      stack.printStack();
-      stack.pop();
-      stack.printStack();
+      stack.push(85);
+      System.err.println(stack.size());
+      
+      stack.display();
+      System.out.println("peek ele"+stack.peek());
+      System.out.println("pop element"+ stack.pop());
+      stack.display();
+   
 
     }
 }
--- JAVA/Strings/Basics.java
+++ JAVA/Strings/Basics.java
@@ -30,5 +30,16 @@ public class Basics {
     // System.out.print(str.endsWith("tel"));
 
 
+
+
+    // sub strings
+    String str = "abcde";
+    // System.out.println(str.substring(2));
+    
+    for(int i=0;i<str.length();i++){
+        for(int j =i+1;j<str.length()+1;j++){
+            System.out.print(str.substring(i,j)+ " ");
+        }
+    }
     }
 }
