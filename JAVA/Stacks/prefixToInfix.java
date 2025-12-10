5eb8735958ca0691b37bbf326ead7b5eef69b1f3
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-12-10 12:27:01 +0530

    add

---
+++ JAVA/Stacks/prefixToInfix.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+public class prefixToInfix {
+    public static void main(String[] args){
+        String str = "-9/*+5346";
+        Stack<String> val  = new Stack<>();
+
+        for(int i = str.length()-1;i>=0;i--){
+            char ch = str.charAt(i);
+            int ascii =(int)ch;
+            if(ascii>=48 && ascii<=57){
+                val.push(ascii-48+"");
+            } else{
+                String v1 = val.pop();
+                String v2 = val.pop();
+                char op = ch;
+
+                // v1 op v2
+                String t = "("+v1+op+v2+")";
+                val.push(t);
+            }
+        }
+        System.out.println(val.pop());
+    }
+}
